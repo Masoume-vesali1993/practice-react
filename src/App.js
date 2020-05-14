f8d231c762d293a1ea52ad3a9154a798cbe8c242
masoumeh vsl <masoumevsl198@gmail.com>
2020-05-14 19:34:11 +0430

    new part react

--- src/App.js
+++ src/App.js
@@ -3,12 +3,7 @@ import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <div>
-        <h1>HI</h1>
-        <p>Hi my friend</p>
-      </div>
-    </div>
+    React.createElement('div', null, 'salam') //besorate react
   );
 }
 
