533824cdbe8b15fe267dc9e1279c525ee04ad281
masoumeh vsl <masoumevsl198@gmail.com>
2020-05-15 17:40:01 +0430

    function
    use in jsx

--- src/Components/User.js
+++ src/Components/User.js
@@ -2,7 +2,7 @@ import React from 'react';
 let User = function (){
     let u = {'name': 'Masoume'};
     return (
-        <h2>some {u.name} text k</h2>
+        <h2>some {(function(){return 'my friennds'; })()} text</h2>
     );
 }
 
