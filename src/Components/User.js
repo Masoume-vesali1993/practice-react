e777a77ea31d6fb25ae129d152350e7eb2d97d65
masoumeh vsl <masoumevsl198@gmail.com>
2020-05-15 17:22:36 +0430

    use
    js in component

--- src/Components/User.js
+++ src/Components/User.js
@@ -1,5 +1,9 @@
+import React from 'react';
 let User = function (){
-    return 'salam';
+    let u = {'name': 'Masoume'};
+    return (
+        <h2>some {u.name} text</h2>
+    );
 }
 
 export default User;
