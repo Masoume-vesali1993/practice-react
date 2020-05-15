04cd09d41fe8cac21ca286facd8be86787ca8fe0
masoumeh vsl <masoumevsl198@gmail.com>
2020-05-15 18:07:11 +0430

    use
    ather tag in App.js

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
       <h1>salam</h1>
       <User name="masoume" age="26">
           <h1>my lastname vsali</h1>
+          <u>some underline text</u>
         </User>
     </div>
   );
