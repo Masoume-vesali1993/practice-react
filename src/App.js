e9dca8674065534afa12334e9530705c70670bdc
masoumeh vsl <masoumevsl198@gmail.com>
2020-05-17 01:49:47 +0430

    use
    in this onButtonClick

--- src/App.js
+++ src/App.js
@@ -11,9 +11,14 @@ class App extends Component {
     ]
   }
 
+  onButtonClick = () => {
+    console.log(this);
+  }
+
   render(){
   return (
     <div>
+      <button onClick={this.onButtonClick}> change state </button>
       <User name= {this.state.users[0].name} age={this.state.users[0].age} />
       <User name={this.state.users[1].name} age={this.state.users[1].age} />
       <User name={this.state.users[2].name} age={this.state.users[2].age} />
