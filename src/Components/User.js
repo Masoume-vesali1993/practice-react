0ff3bea265e1373e6c4d82ef01fd1b00c0c07b4b
masoumeh vsl <masoumevsl198@gmail.com>
2020-05-17 03:06:00 +0430

    part tow
    user state less

--- src/Components/User.js
+++ src/Components/User.js
@@ -1,27 +1,27 @@
 import React, { Component } from 'react';
 
-class User extends Component  {
-    render(){
-        return(
-            <div>
-            some be {this.props.name}<br />
-            shoma {this.props.age} sal darid<br/>
-            {this.props.children}
-            </div>
-        );
-    }
-}
-
-
-// let User = function (props){
-
-//     return (
-//         <div>
-//             some be {props.name}<br />
-//             shoma {props.age} sal darid<br/>
-//             {props.children}
-//         </div>
-//     );
+// class User extends Component  {
+//     render(){
+//         return(
+//             <div>
+//             some be {this.props.name}<br />
+//             shoma {this.props.age} sal darid<br/>
+//             {this.props.children}
+//             </div>
+//         );
+//     }
 // }
+
+
+let User = function (props){
+
+    return (
+        <div>
+            some be {props.name}<br />
+            shoma {props.age} sal darid<br/>
+            {props.children}
+        </div>
+    );
+}
 
 export default User;
