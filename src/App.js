56f392fee47dedef7ef08be82f43fd3771b7ac44
masoumeh vsl <masoumevsl198@gmail.com>
2020-05-16 17:02:58 +0430

    dont use
    dobel in jsx

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
   render(){
   return (
     <div>
-      <User name="{this.state.user[0].name}" age="26" />
+      <User name= {this.state.users[0].name} age="26" />
       <User name="parham" age="6" />
       <User name="arshia" age="9" />
     </div>
