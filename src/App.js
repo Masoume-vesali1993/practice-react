0b43ca1d578ed2da74417641edb712a672b821fc
masoumeh vsl <masoumevsl198@gmail.com>
2020-05-16 16:57:11 +0430

    use
    new state

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import User from './Components/User';
 
 
-function App() {
+class App extends Component {
+  state = {
+    users : [
+      {name:"masoume" ,age:"26"},
+      {name:"parham", age:"6"},
+      {name:"arshia", age:"9"},
+    ]
+  }
+
+  render(){
   return (
     <div>
-      <User name="masoume" age="26" />
+      <User name="{this.state.user[0].name}" age="26" />
       <User name="parham" age="6" />
       <User name="arshia" age="9" />
     </div>
-  );
+    );
+  }
 }
 
 export default App;
