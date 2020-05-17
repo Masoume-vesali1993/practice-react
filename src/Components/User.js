cf1637bb5895465423bdc16ef9e47f778f4f6519
masoumeh vsl <masoumevsl198@gmail.com>
2020-05-17 22:22:04 +0430

    add
    text

--- src/Components/User.js
+++ src/Components/User.js
@@ -24,21 +24,12 @@ export default class User extends Component  {
                 some be {this.props.name}<br />
                 shoma {this.props.age} sal darid<br/>
                 {this.props.children}
+
+                <h2>some text</h2>
+                <h3>another text</h3>
             </div>
         );
     }
 }
 
 
-// let user = function (props){
-
-//     return (
-//         <div>
-//             some be {props.name}<br />
-//             shoma {props.age} sal darid<br/>
-//             {props.children}
-//         </div>
-//     );
-// }
-
-// export default user;
