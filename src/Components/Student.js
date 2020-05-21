aaea978d4d9794b6337c7e96b918acf8e0058315
masoumeh vsl <masoumevsl198@gmail.com>
2020-05-21 21:53:57 +0430

    add
    new Component

---
+++ src/Components/Student.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+export default class Student extends Comment{
+    render(){
+        return(
+            <div>
+                noting
+            </div>
+        );
+    }
+}
