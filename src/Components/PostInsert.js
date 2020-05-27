db2a2a9b639e688ccd32a50f0b37dfe2257b42f6
masoumeh vsl <masoumevsl198@gmail.com>
2020-05-27 13:07:31 +0430

    Add
    form in list

--- src/Components/PostInsert.js
+++ src/Components/PostInsert.js
@@ -4,9 +4,13 @@ import React, {Component} from 'react';
 class PostInsert extends Component{
     onPostCreate = (e) => {
         e.preventDefult();
+        let form = e.target;
+        console.log(form);
+        return;
         let post = {title: 'new', body: 'all'};
         this.props.onPostCreate(post);
     }
+
     render (){
         return (
             <div className="post-insert">
