1b48997bd59c92d5a050f38497c11e25efb6f799
masoumeh vsl <masoumevsl198@gmail.com>
2020-05-27 09:27:50 +0430

    Edit
    file

--- src/App.js
+++ src/App.js
@@ -1,40 +1,14 @@
 import React, { Component } from 'react';
-import Student from './Components/Student';
+
 
 
 class App extends Component {
-  state = {
-    students : [
-      {id: 1, name:'Masoume'},
-      {id: 2, name:'Zahra'},
-      {id: 3, name:'Fateme'},
-    ],
-
-    randomUserId: 0
-  }
-
-  randomUserHidden = () => {
-    let newState = {...this.state};
-    newState.randomUserId = Math.ceil(Math.random() *3);
-    this.setState(newState);
-  }
+  
 
   render(){
     return (
       <div> 
-        <button onClick={this.randomUserHidden}>hide random state</button>
-        {
-          this.state.students
-          .filter(
-            s => s.id != this.state.randomUserId
-          )
-
-          .map(
-            s => ( 
-            <Student name={s.name} />
-            )
-          )
-        }
+        salam
       </div>
     );
   }
