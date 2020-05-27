c2f431fa0bd5fbb7f24b7e74602dd9a2aa7ba303
masoumeh vsl <masoumevsl198@gmail.com>
2020-05-27 19:15:15 +0430

    tow part
    for in shoe element

--- src/Components/PostInsert.js
+++ src/Components/PostInsert.js
@@ -2,13 +2,21 @@ import React, {Component} from 'react';
 
 
 class PostInsert extends Component{
+    titleInput=null;
+    bodyInput =null;
     onPostCreate = (e) => {
         e.preventDefault();
 
+        // let post = {
+        //     title: e.target.elements.title.value,
+        //     body: e.target.elements.body.value
+        // };
+
         let post = {
-            title: e.target.elements.title.value,
-            body: e.target.elements.body.value
+            title: this.titleInput.value,
+            body: this.bodyInput.value
         };
+
         this.props.onPostCreate(post);
     }
 
@@ -17,12 +25,14 @@ class PostInsert extends Component{
             <div className="post-insert">
                 <form onSubmit={this.onPostCreate}>
                     <div>
-                        <input name="title" placeholder="post title" />
+                        {/* <input name="title" placeholder="post title" /> */}
+                        <input ref={el => this.titleInput = el } placeholder="post title" />
                     </div>
 
 
                     <div>
-                        <textarea name="body" placeholder="post body"></textarea>
+                        {/* <textarea name="body" placeholder="post body"></textarea> */}
+                        <textarea ref={el => this.bodyInput = el } placeholder="post body"></textarea>
                     </div>
 
                     <button type="submit">create</button>
