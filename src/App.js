09718015ab8a5d1f9845dd298539b26945f15912
masoumeh vsl <masoumevsl198@gmail.com>
2020-05-27 09:35:29 +0430

    New
    state

--- src/App.js
+++ src/App.js
@@ -3,12 +3,20 @@ import React, { Component } from 'react';
 
 
 class App extends Component {
+  state = {
+    title: 'my project title ',
+    posts: [
+      {id:1, title: 'post 1', body: 'post 1 body'},
+      {id:1, title: 'post 2', body: 'post 2 body'},
+      {id:1, title: 'post 3', body: 'post 3 body'},
+    ]
+  }
   
 
   render(){
     return (
       <div> 
-        salam
+        <h1>{this.state.title}</h1>
       </div>
     );
   }
