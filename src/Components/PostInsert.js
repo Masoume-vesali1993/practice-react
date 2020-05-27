5e6a6e0fe463abe128453b92db5141ce6bdd286b
masoumeh vsl <masoumevsl198@gmail.com>
2020-05-27 10:25:51 +0430

    Add
    form in component

--- src/Components/PostInsert.js
+++ src/Components/PostInsert.js
@@ -1,10 +1,23 @@
 import React, {Component} from 'react';
 
+
 class PostInsert extends Component{
     render (){
         return (
             <div className="post-insert">
-                PostInsert
+                <form>
+                    <div>
+                        <input placeholder="post title" />
+                    </div>
+
+
+                    <div>
+                        <textarea placeholder="post body"></textarea>
+                    </div>
+
+                    <button type="submit">create</button>
+
+                </form>
             </div>
         )
     }
