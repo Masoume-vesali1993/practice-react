ea9dd2a93f325ed3fadd524f831f404d9cf58546
masoumeh vsl <masoumevsl198@gmail.com>
2020-06-02 10:29:34 +0430

    use props in post

--- src/App.js
+++ src/App.js
@@ -11,6 +11,8 @@ class App extends Component {
   state = {
     posts: [
       {id:1, title: 'post 1', body: 'post 1 body'},
+      {id:2, title: 'post 2', body: 'post 2 body'},
+      {id:3, title: 'post 3', body: 'post 3 body'},
     ]
   }
   
--- src/Components/Post.js
+++ src/Components/Post.js
@@ -1,14 +1,18 @@
 import React, { component, Component } from 'react';
-import { withRouter } from 'react-router-dom';
+import { withRouter,Link } from 'react-router-dom';
 
 class Post extends Component {
     render() {
-        console.log('[post]',this.props);
+        console.log(this.props);
         return (
-            <div className="post">
-              <h3>{this.props.title}</h3>
-              <p>{this.props.body}</p>
-            </div>
+            <Link to={{
+                pathname: "/blog"
+            }}>
+                <div className="post">
+                <h3>{this.props.title}</h3>
+                <p>{this.props.body}</p>
+                </div>
+            </Link>
         );
     }
 }
