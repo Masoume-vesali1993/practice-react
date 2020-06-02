e40ce63cb327f3b66ffacd38fe2d82d91def2918
masoumeh vsl <masoumevsl198@gmail.com>
2020-06-02 11:13:09 +0430

    dont use in link

--- src/Components/SinglePost.js
+++ src/Components/SinglePost.js
@@ -1,12 +1,11 @@
-import React, { component, Component } from 'react';
+import React, { Component } from 'react';
 
 class SinglePost extends Component {
     render() {
         console.log(this.props);
         return (
             <div className="post">
-                <Link to={{ pathname: this.props.match.path + "/" + this.props.id}}> <h3>{this.props.title}</h3>
-                </Link>
+                <h3>{this.props.title}</h3>
                 <p>{this.props.body}</p>
             </div>
         );
