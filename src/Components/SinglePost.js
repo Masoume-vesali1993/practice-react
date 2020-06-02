dd402c767ae5036060d5f3fc48c3631153d3a707
masoumeh vsl <masoumevsl198@gmail.com>
2020-06-02 12:08:59 +0430

    use if in page singlepage

--- src/Components/SinglePost.js
+++ src/Components/SinglePost.js
@@ -3,12 +3,17 @@ import React, { Component } from 'react';
 class SinglePost extends Component {
     render() {
         console.log('[Single-post]');
-        return (
-            <div className="single-post">
-                <h3>{this.props.post.title}</h3>
-                <p>{this.props.post.body}</p>
-            </div>
-        );
+        if(this.props.post){
+            return (
+                <div className="single-post">
+                    <h3>{this.props.post.title}</h3>
+                    <p>{this.props.post.body}</p>
+                </div>
+            );
+        }
+        else{
+            return null;
+        }
     }
 }
 
