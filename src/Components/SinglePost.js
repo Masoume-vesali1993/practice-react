784e4ee175c565ee638f8d5be59f09443b30e930
Masoume <masoumevsl198@gmail.com>
2020-06-11 09:54:28 -0700

    curect
    post

--- src/Components/SinglePost.js
+++ src/Components/SinglePost.js
@@ -23,7 +23,7 @@ class SinglePost extends Component {
 
     componentDidMount(){
             axios
-            .get('https://jsonplaceholder.typicode.com/postsd/' + this.props.id)
+            .get('https://jsonplaceholder.typicode.com/posts/' + this.props.id)
             .then(response => {
                 let post = {
                     id: response.data.id,
