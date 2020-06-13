6472378217c04f64839bb47a687189824099129c
Masoume <masoumevsl198@gmail.com>
2020-06-12 22:39:22 -0700

    show
    post

--- src/Components/NewPost.js
+++ src/Components/NewPost.js
@@ -20,7 +20,7 @@ class NewPost extends Component {
 
             console.log(post);
 
-            axios.post('https://jsonplaceholder.typicode.com/postss', post)
+            axios.post('https://jsonplaceholder.typicode.com/posts', post)
             .then(response => {
                 console.log(response);
             })
