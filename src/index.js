c4e57fa7251ca4c71e3e385b010df0e437308102
Masoume <masoumevsl198@gmail.com>
2020-06-12 23:56:25 -0700

    add new request
    Signed-off-by: Masoume <masoumevsl198@gmail.com>

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import axios from 'axios';
 
 axios.interceptors.request.use(
   request => {
+    request.headers['Foo'] = 'bar';
     console.log('[request interceptor :: success]' , request);
     return request;
   }
