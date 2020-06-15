947278acc0fe7945d034fe18fec535b840c2de1e
Masoume <masoumevsl198@gmail.com>
2020-06-15 01:06:06 -0700

    add redux
    Signed-off-by: Masoume <masoumevsl198@gmail.com>

--- src/redux-test.js
+++ src/redux-test.js
@@ -1,1 +1,16 @@
-console.log('redux')
+import { createStore } from 'redux';
+//1- set initial state
+const initState = {
+    name: 'Masoume'
+};
+
+//2- create a reducer
+const reducer = (state = initState, action) => {
+    return state;
+}
+
+//3- create a store
+const store = createStore(reducer);
+
+//get store current store
+console.log(store.getState());
