5afceec0ae8aa7471b4fc9b6116a7cfe19ef5454
Masoume <masoumevsl198@gmail.com>
2020-06-16 00:38:43 -0700

    use () for type
    Signed-off-by: Masoume <masoumevsl198@gmail.com>

--- src/redux-test-action.js
+++ src/redux-test-action.js
@@ -3,8 +3,8 @@ export const INC_ACTION = 'INCREMENT';
 export const DEC_ACTION = 'DECREMENT';
 
 
-export const incrementAction = () => {type: INC_ACTION};
+export const incrementAction = () => ({type: INC_ACTION});
 
-export const decrementActiion = () => {type:DEC_ACTION};
+export const decrementActiion = () => ({type:DEC_ACTION});
 
-export const addAction = () => {type: ADD_ACTION , value: 10};
+export const addAction = () => ({type: ADD_ACTION , value: 10});
