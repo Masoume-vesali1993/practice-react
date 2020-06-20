5cc93cc46ea169f2707ebf40a913ec4c4a84b629
Masoume <masoumevsl198@gmail.com>
2020-06-20 10:07:34 +0430

    add item in todo
    Signed-off-by: Masoume <masoumevsl198@gmail.com>

---
+++ src/redux-test.js
@@ -0,0 +1,29 @@
+import { createStore } from 'redux';
+
+//create store
+    //initial state
+    const initState = {
+        todo: []
+    };
+
+    //create reducer
+    const reducer = (state = initState, action) => {
+        if(action.type === 'ADD_ITEM'){
+            return {
+                ...state,
+                todo: [...state.todo, action.item]
+            };
+        }
+        return state;
+    }
+
+    //create store
+    const store = createStore(reducer);
+
+    //listen to store
+    store.subscribe(() => {
+        console.log(store.getState());
+    });
+
+//dispatch actions
+    store.dispatch({type: 'ADD_ITEM', item: {title: 'todo 1', body: 'fffff'}});
