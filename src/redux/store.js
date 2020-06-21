9effc7c40ee1547f6a1a172ac2f74b1244c0bea8
Masoume <masoumevsl198@gmail.com>
2020-06-21 12:32:03 +0430

    Composition reducers and store  subscriber
    Signed-off-by: Masoume <masoumevsl198@gmail.com>

--- src/redux/store.js
+++ src/redux/store.js
@@ -5,20 +5,19 @@ import reducer2 from './reducer2';
 import * as Acts1 from './store1-actions';
 import * as Acts2 from './reducer2-actions';
 
-const reducer = combineReducers();
-const store = createStore(reducer1);
+const reducer = combineReducers({
+    r1:reducer1,
+    r2:reducer2
+});
+
+const store = createStore(reducer);
 
 store.subscribe(() => {
-    console.log('[store 1 subscriber]' , store.getState());
+    console.log('[store  subscriber]' , store.getState());
 })
 
 store.dispatch(Acts1.addUser({id: 2, name: 'parham', family: 'vesali'}));
 
 
-// const store = createStore(reducer);
 
-// store.subscribe(() => {
-//     console.log('[store 1 subscriber]' , store.getState());
-// })
-
-// store.dispatch(Acts.addUser({id: 2, name: 'parham', family: 'vesali'}));
+store.dispatch(Acts2.addProduct({id: 2, name: 'parham', family: 'vesali'}));
