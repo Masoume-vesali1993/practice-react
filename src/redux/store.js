be3429fedaa7e68b5c59f46783ed6ac87de0f3d4
Masoume <masoumevsl198@gmail.com>
2020-06-21 12:20:53 +0430

    new store and subscribe store1 and show users
    Signed-off-by: Masoume <masoumevsl198@gmail.com>

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,16 +2,16 @@ import { createStore } from 'redux';
 import reducer1 from './store1';
 import reducer2 from './reducer2';
 
-import Acts1 from './store1-actions';
-import Acts2 from './reducer2-actions';
+import * as Acts1 from './store1-actions';
+import * as Acts2 from './reducer2-actions';
 
-const store = createStore(reducer);
+const store = createStore(reducer1);
 
 store.subscribe(() => {
     console.log('[store 1 subscriber]' , store.getState());
 })
 
-store.dispatch(Acts.addUser({id: 2, name: 'parham', family: 'vesali'}));
+store.dispatch(Acts1.addUser({id: 2, name: 'parham', family: 'vesali'}));
 
 
 // const store = createStore(reducer);
