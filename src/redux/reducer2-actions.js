8e8f78c09d8e08c53f7f754bd0a63b66ccab4928
Masoume <masoumevsl198@gmail.com>
2020-06-21 11:57:48 +0430

    add new product
    Signed-off-by: Masoume <masoumevsl198@gmail.com>

--- src/redux/reducer2-actions.js
+++ src/redux/reducer2-actions.js
@@ -1,3 +1,3 @@
-export const PRODUCT = 'ADD_PRODUCT';
+export const ADD_PRODUCT = 'ADD_PRODUCT';
 
 export const addProduct = (product) => ({type: ADD_PRODUCT, product});
--- src/redux/reducer2.js
+++ src/redux/reducer2.js
@@ -3,8 +3,8 @@ import * as Acts from './reducer2-actions';
 
 const initialState = {
     products: [
-        {id:1 , title: 'product1' , price: 25000 }
-        {id:2 , title: 'product2' , price: 45000 }
+        {id:1 , title: 'product1' , price: 25000 },
+        {id:2 , title: 'product2' , price: 45000 },
     ]
 };
 
