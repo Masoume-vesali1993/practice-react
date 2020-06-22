236ec45782e3a9661c0e567e91e67b4cc967c13c
Masoume <masoumevsl198@gmail.com>
2020-06-22 12:29:27 +0430

    add part AddTodo
    Signed-off-by: Masoume <masoumevsl198@gmail.com>

---
+++ src/components/AddTodo.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+
+class AddTodo extends Component{
+    state = {
+        enableButton: false
+    }
+
+    inputBox = null;
+
+    addTodoHandler = () => {
+        this.props.addItem({
+            title: this.inputBox.value
+        });
+        this.inputBox.value = '';
+        this.inputChange();
+    }
+
+    inputChange = () => {
+        this.setState((oldState) => ({
+            ...oldState,
+            enableButton: this.inputBox.value.trim() !== ''
+        }));
+    }
+    
+    render() {
+        return (
+            <div className="add-todo">
+                <input ref={el => this.inputBox=el} onChange={this.inputChange} />
+                <button onClick={this.addTodoHandler} disabled={!this.state.enableButton}>AddChange</button>
+            </div>
+        );
+    }
+}
+
+export default AddTodo;
