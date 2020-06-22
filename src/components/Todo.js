64638951b95d0695764e325865bf4323f97fcf29
Masoume <masoumevsl198@gmail.com>
2020-06-22 11:47:17 +0430

    add redux in app react
    Signed-off-by: Masoume <masoumevsl198@gmail.com>

---
+++ src/components/Todo.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import AddTodo from './AddTodo';
+
+class Todo extends Component{
+    state = {
+        items: [
+            {id:1, title: 'todo 1', done: false},
+            {id:2, title: 'todo 2', done: false},
+        ]
+
+    }
+
+    renderItems = () => {
+
+    }
+
+    getNextId = () => {
+        
+    }
+
+    addTodo = (todo) => {
+        
+    }
+
+    deleteTodo = (id) => {
+        
+    }
+
+    render() { 
+
+    }
+}
+
+export default Todo;
