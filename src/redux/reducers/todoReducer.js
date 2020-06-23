16e7d79c2779618cd13d7268717698f666beeed8
Masoume <masoumevsl198@gmail.com>
2020-06-23 13:01:18 +0430

    add Action Functionall and Add reducer
    Signed-off-by: Masoume <masoumevsl198@gmail.com>

--- src/redux/reducers/todoReducer.js
+++ src/redux/reducers/todoReducer.js
@@ -1,2 +1,32 @@
 const ADD_TODO_ACTION = 'ADD';
 const REMOVE_TODO_ACTION = 'REMOVE_TODO';
+
+const addTodo = () => ({
+    'type': ADD_TODO_ACTION,
+    todo
+});
+
+const removeTodo = (id) => ({
+    'type' : REMOVE_TODO_ACTION,
+    id
+});
+
+const todoReducer = (state = {}, action) => {
+    switch(action.type){
+        case ADD_TODO_ACTION:
+            console.log('add todo action', action.todo);
+            break;
+        case REMOVE_TODO_ACTION:
+            console.log('remove todo action', action.id);
+            break;
+    }
+
+    return state;
+}
+
+export {
+    addTodo,
+    removeTodo
+};
+
+export default todoReducer;
