15de2a6a44878d283362ca54e0be22b40c618bb5
Masoume <masoumevsl198@gmail.com>
2020-06-25 15:14:42 +0430

    dont use state
    Signed-off-by: Masoume <masoumevsl198@gmail.com>

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -4,15 +4,6 @@ import AddTodo from './AddTodo';
 import  {addTodo, removeTodo} from '../redux/todoStore';
 
 class Todo extends Component{
-    state = {
-        
-        items: [
-            {id:1, title: 'todo 1', done: false},
-            {id:2, title: 'todo 2', done: false},
-        ]
-
-    }
-
     renderItems = () => {
         return this.state.items.map(item => (
             <div key={item.id}>
