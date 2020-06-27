b9a8c88e9c821bd9d44f4aaba770fffa7444aa09
Masoume <masoumevsl198@gmail.com>
2020-06-27 12:46:36 +0430

    start counter
    Signed-off-by: Masoume <masoumevsl198@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import Counter from './components/Counter';
 
 
 class App extends Component {
     render() {
       return (
         <div className='app'>
-          sample 01 -counter
+          <Counter />
         </div>
       );
     }
