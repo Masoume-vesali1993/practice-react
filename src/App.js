40a8c087c7ccc26a2c804dbef60ac19aef31e913
Masoume <masoumevsl198@gmail.com>
2020-06-27 15:45:35 +0430

    Counter conect  in store and show on provider in store
    Signed-off-by: Masoume <masoumevsl198@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import Counter from './components/Counter';
-
+import { Provider } from 'react-redux';
+import CounterStore from './redux/CounterStore';
 
 class App extends Component {
     render() {
       return (
-        <div className='app'>
-          <Counter />
-        </div>
+        <Provider store={CounterStore}>
+          <div className='app'>
+            <Counter />
+          </div>
+        </Provider>
       );
     }
  }
--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,15 +1,12 @@
 import React, { Component } from 'react';
-import { Provider } from 'react-redux';
-import CounterStore from '../redux/CounterStore';
+
 
 class Counter extends Component{
     render(){
         return(
-            <Provider store={CounterStore}>
                 <div className="counter">
                     0
                 </div>
-            </Provider>
         )
     }
 }
