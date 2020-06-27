3a25993380957ac6f4dbd38d54596a23f1541bcd
Masoume <masoumevsl198@gmail.com>
2020-06-27 18:05:08 +0430

    add ather counter
    Signed-off-by: Masoume <masoumevsl198@gmail.com>

---
+++ src/components/CounterButtons.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class CounterButtons extends Component{
+    render(){
+        return (
+            <div className="counter-buttons">
+
+            </div>
+        );
+    }
+}
+
+export default CounterButtons;
