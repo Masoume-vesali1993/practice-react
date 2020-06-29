7672ebda26735021110d3872b9c5d7f0385c592b
Masoume <masoumevsl198@gmail.com>
2020-06-29 20:12:26 +0430

    correct component and show data
    Signed-off-by: Masoume <masoumevsl198@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import Shop from './components/Shop';
+
 
 class App extends Component {
     render() {
       return (
           <div className='app'>
-            example 2 - shoping card
+            <Shop />
           </div>
       );
     }
---
+++ src/components/Shop.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Shop extends Component {
+
+    render() {
+        return (
+            <div className='shop'>
+                shop...
+            </div>
+        )
+    }
+}
+
+export default Shop;
