6bb75110bd40724517b8151f8226c4746852c233
Masoume <masoumevsl198@gmail.com>
2020-06-29 18:39:53 +0430

    add reducer
    Signed-off-by: Masoume <masoumevsl198@gmail.com>

---
+++ src/redux/reducers/ShopReducer.js
@@ -0,0 +1,16 @@
+
+
+const initialState = {
+    items: [
+        {id:1, name: 'tv', price: 1500000},
+        {id:1, name: 'radio', price: 100000},
+        {id:1, name: 'dvd player', price: 1700000},
+    ]
+}
+const reducer = (state = initialState, action) => {
+
+    return state;
+
+}
+
+export default reducer;
