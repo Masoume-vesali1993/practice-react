e75f9d31ace6d84fbfc2fe7c87c8b876403a9e8b
Masoume <masoumevsl198@gmail.com>
2020-06-29 17:52:35 +0430

    new work shoping card
    Signed-off-by: Masoume <masoumevsl198@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
 import React, { Component } from 'react';
-import Counter from './components/Counter';
-import { Provider } from 'react-redux';
-import CounterStore from './redux/CounterStore';
-import CounterButtons from './components/CounterButtons';
 
 class App extends Component {
     render() {
       return (
-        <Provider store={CounterStore}>
           <div className='app'>
-            <Counter />
-            <CounterButtons />
+            example 2 - shoping card
           </div>
-        </Provider>
       );
     }
  }
